1e0fbcd637da26f55778b407989a72e4ca5413f1
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2022-09-28 02:21:46 +0200

    fix centralized logic

--- Activity-01/src/main/java/smart_room/centralized/CentralizedSingleBoardSimulator.java
+++ Activity-01/src/main/java/smart_room/centralized/CentralizedSingleBoardSimulator.java
@@ -1,36 +1,48 @@
 package smart_room.centralized;
 
+import smart_room.Event;
+
 public class CentralizedSingleBoardSimulator {
     private static final double T_VALUE = 0.3;
+    static boolean status = false;
 
     public static void main(String[] args) {
         SinglelBoardSimulator board = new SinglelBoardSimulator();
         board.init();
 
-        // Event-loop
-
-        /* board.register((Event ev) -> {
-            if (ev instanceof PresenceDetected) {
-                if (board.getLuminosity() < T_VALUE) {
-                    board.on();
-                }
-            } else if (ev instanceof PresenceNoMoreDetected) {
-                board.off();
-            }
-        }); */
-
-
         // Super-loop
 
-        new Thread(() -> {
+        /*new Thread(() -> {
             while (true) {
                 if (!board.presenceDetected())
                     board.off();
                 else
                     if (board.getLuminosity() < T_VALUE)
                         board.on();
+                    else
+                        board.off();
             }
-        }).start();
+        }).start();*/
+
+        // Event-loop
+        board.register((Event ev) -> {
+            if (ev instanceof PresenceDetected && board.getLuminosity() < T_VALUE) {
+                status = true;
+                board.on();
+            } else if (ev instanceof PresenceDetected && board.getLuminosity() >= T_VALUE) {
+                status = true;
+                board.off();
+            } else if (ev instanceof PresenceNoMoreDetected) {
+                status = false;
+                board.off();
+            } else{
+                if(status && board.getLuminosity() < T_VALUE){
+                    board.on();
+                }else {
+                    board.off();
+                }
+            }
+        });
     }
 
 }
